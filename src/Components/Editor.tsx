62efb5a098b5e6763bdaf1b1a9ab2322d7755eee
ecuber <37519236+ecuber@users.noreply.github.com>
2021-03-17 03:15:44 -0400

    Fix tooltip

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import ReactQuill from 'react-quill'
 import { useMediaQuery } from 'react-responsive'
-import { Popover } from 'react-text-selection-popover'
 import styled from 'styled-components'
 
 const styles: React.CSSProperties = {
@@ -29,9 +28,30 @@ const Editor: React.FC<PropTypes> = (props: PropTypes) => {
     setValue(val)
     props.onChange(val)
   }
+  const [bounds, setBounds] = useState({ left: 0, top: 0, height: 0, width: 0 })
+  const [text, setText] = useState('')
 
   const isMobile = useMediaQuery({ query: '(max-width: 760px)' })
   const height = isMobile ? 300 : 445
+
+  const Popover: React.FC = () => {
+    if (!text) { return null }
+    const Tooltip = styled.div`
+      position: absolute;
+      left: ${bounds.left + bounds.width / 2}px;
+      top: ${140 + bounds.top}px;
+      margin-left: -75px;
+      width: auto;
+      height: 1.4em;
+      background: #5dd9b2;
+      font-size: 1em;
+      pointer-events: none;
+      text-align: center;
+      color: white;
+      border-radius: 3px;
+    `
+    return <Tooltip>selected <strong>{props.countWords(text || '')}</strong> words</Tooltip>
+  }
 
   return (
     <>
@@ -42,25 +62,14 @@ const Editor: React.FC<PropTypes> = (props: PropTypes) => {
       theme='bubble'
       defaultValue={value || ''}
       onChange={setVal}
-      modules={{ toolbar: false }} />
-      <Popover render={({ clientRect, isCollapsed, textContent }) => {
-        if (clientRect == null || isCollapsed) { return null }
-        const Tooltip = styled.div`
-          position: absolute;
-          left: ${clientRect.left + clientRect.width / 2}px;
-          top: ${clientRect.top - 40}px;
-          margin-left: -75px;
-          width: 170px;
-          height: 1.4em;
-          background: #5dd9b2;
-          font-size: 1em;
-          pointer-events: none;
-          text-align: center;
-          color: white;
-          border-radius: 3px;
-        `
-        return <Tooltip>selected <strong>{props.countWords(textContent || '')}</strong> words</Tooltip>
-      }}/>
+      onChangeSelection={(range, source, editor) => {
+        if (!range) { setBounds({ left: 0, top: 0, height: 0, width: 0 }) } else {
+          setBounds(editor.getBounds(range.index, range.length))
+          setText(editor.getText(range.index, range.length))
+        }
+      }}
+      modules={{ toolbar: false }}/>
+      <Popover/>
     </>
   )
 }
